69e4ea252f63513e55e0f0a0168ac5c15c088f09
Roman <pliskarv@yandex.ru>
2024-01-07 01:15:41 +0900

    выполнил все 4 задачи

--- homework_2/Program.cs
+++ homework_2/Program.cs
@@ -1,2 +1,28 @@
 ﻿// Задача 2: Напишите программу, которая принимает на вход координаты точки (X и Y), 
 // причём X ≠ 0 и Y ≠ 0 и выдаёт номер координатной четверти плоскости, в которой находится эта точка.
+
+Console.Write ("Enter the coordinates of the point x and y by a space: ");
+string [] coordinates = Console.ReadLine ().Split('');
+int X = Convert.ToInt32 (coordinates[0]);
+int y = Convert.ToInt32 (coordinates[1]);
+
+if (x > 0 && y > 0)
+{
+    Console.WriteLine ("1");
+}
+else if (x < 0 && y > 0)
+{
+    Console.WriteLine ("2");
+}
+else if (x < 0 && y < 0)
+{
+    Console.WriteLine ("3");
+}
+else if (x > 0 && y < 0)
+{
+    Console.WriteLine ("4");
+}
+else
+{
+    Console.WriteLine ("a point on the coordinate axis");
+}
--- homework_4/Program.cs
+++ homework_4/Program.cs
@@ -1,1 +1,23 @@
 ﻿// Задача 4: Напишите программу, которая на вход принимает натуральное число N, а на выходе показывает его цифры через запятую.
+
+Console.Write ("Enter the natural number: ");
+int n = Convert.ToInt32 (Console.ReadLine ());
+
+if (n<10)
+{
+    Console.WriteLine (n);
+}
+else
+{
+    write (n > 0);
+}
+int currentDigit = n % 10;
+n /= 10;
+if (n>0)
+{
+    Console.Write(currentDigit+",");
+}
+else
+{
+    Console.WriteLine(currentDigit);
+}
